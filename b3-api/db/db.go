84cfa114507a40c9d0134809c3f2afb334193a34
Rafael Silva Menezes <rafasilva320.rs@gmail.com>
2024-07-12 10:48:10 -0300

    update .gitignore

--- b3-api/db/db.go
+++ b3-api/db/db.go
@@ -25,7 +25,6 @@ func InitDB() {
 
 	log.Println("Conectado ao banco de dados PostgreSQL...")
 
-	// Criar índices no banco de dados, se necessário
 	_, err = db.Exec(`
 		CREATE INDEX IF NOT EXISTS idx_ticker ON trades(ticker);
 		-- Adicione outros índices conforme necessário para trade_price, traded_quantity, etc.
